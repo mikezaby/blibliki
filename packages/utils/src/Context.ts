a9fa9b0a1f5f7a25703a9689978c7e2785042adf
Michalis Zabaras <mikezaby@gmail.com>
2025-10-26 16:07:47 +0200

    Implement AudioContext wrapper
    Currently, this wrapper is completely dummy, but the reason of this is to
    have the ability to enrich with hooks or other staff in the future.

---
+++ packages/utils/src/Context.ts
@@ -0,0 +1,43 @@
+export type AnyAudioContext = AudioContext | OfflineAudioContext;
+
+export class Context {
+  audioContext: AnyAudioContext;
+
+  constructor(context?: AnyAudioContext) {
+    this.audioContext = context ?? new AudioContext();
+  }
+
+  isOffline(): boolean {
+    return this.audioContext instanceof OfflineAudioContext;
+  }
+
+  isOnline(): boolean {
+    return this.audioContext instanceof AudioContext;
+  }
+
+  get destination() {
+    return this.audioContext.destination;
+  }
+
+  get currentTime() {
+    return this.audioContext.currentTime;
+  }
+
+  async close() {
+    if (this.audioContext instanceof OfflineAudioContext) return;
+
+    await this.audioContext.close();
+  }
+
+  async resume() {
+    await this.audioContext.resume();
+  }
+
+  async addModule(url: string | URL) {
+    await this.audioContext.audioWorklet.addModule(url);
+  }
+
+  newAudioWorklet(name: string) {
+    return new AudioWorkletNode(this.audioContext, name);
+  }
+}
