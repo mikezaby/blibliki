e1e779a48becf197dcdb48f12a8ca37e15a09a91
Michalis Zabaras <mikezaby@gmail.com>
2025-12-07 20:45:29 +0200

    Update utils tsup config

--- packages/utils/tsup.config.ts
+++ packages/utils/tsup.config.ts
@@ -6,12 +6,8 @@ export default defineConfig({
     index: "src/index.ts",
     "index.browser": "src/index.browser.ts",
     "index.node": "src/index.node.ts",
-    "Context.browser": "src/Context.browser.ts",
-    "Context.node": "src/Context.node.ts",
     "web-audio-api.browser": "src/web-audio-api.browser.ts",
     "web-audio-api.node": "src/web-audio-api.node.ts",
   },
   ...baseConfig,
-  // Mark node-web-audio-api as external so it's not bundled
-  external: ["node-web-audio-api"],
 });
