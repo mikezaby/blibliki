ceb7a2c42b73811231c4a265ffc285107c5917d6
Michalis Zabaras <mikezaby@gmail.com>
2025-12-13 23:19:27 +0200

    Update vite tanstack router plugin

--- apps/grid/vite.config.ts
+++ apps/grid/vite.config.ts
@@ -1,13 +1,16 @@
 import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 
 const config = defineConfig({
   plugins: [
-    TanStackRouterVite(),
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
     devtools(),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
