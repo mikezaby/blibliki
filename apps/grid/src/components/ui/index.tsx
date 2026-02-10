03182ef24ec4cfdf03740c70d6bdf7d9d5e0c02b
Michalis Zabaras <mikezaby@gmail.com>
2026-02-10 20:15:43 +0200

    chore(grid): remove unused radix dropdown and context menu wrappers

--- apps/grid/src/components/ui/index.tsx
+++ apps/grid/src/components/ui/index.tsx
@@ -1,23 +1,6 @@
 export { Input } from "./input";
 export { Label } from "./label";
 export {
-  DropdownMenu,
-  DropdownMenuTrigger,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuCheckboxItem,
-  DropdownMenuRadioItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuShortcut,
-  DropdownMenuGroup,
-  DropdownMenuPortal,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
-  DropdownMenuRadioGroup,
-} from "./dropdown-menu";
-export {
   Dialog,
   DialogPortal,
   DialogOverlay,
@@ -42,24 +25,6 @@ export {
   SelectScrollUpButton,
   SelectScrollDownButton,
 } from "./select";
-
-export {
-  ContextMenu,
-  ContextMenuTrigger,
-  ContextMenuContent,
-  ContextMenuItem,
-  ContextMenuCheckboxItem,
-  ContextMenuRadioItem,
-  ContextMenuLabel,
-  ContextMenuSeparator,
-  ContextMenuShortcut,
-  ContextMenuGroup,
-  ContextMenuPortal,
-  ContextMenuSub,
-  ContextMenuSubContent,
-  ContextMenuSubTrigger,
-  ContextMenuRadioGroup,
-} from "./context-menu";
 
 export {
   Card,
