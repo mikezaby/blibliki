aaf78ea4de1caf80b29176ae052ab4e29fcff1ab
Michalis Zabaras <mikezaby@gmail.com>
2026-03-07 23:01:21 +0200

    Adjust BaseController

--- packages/engine/src/core/midi/controllers/BaseController.ts
+++ packages/engine/src/core/midi/controllers/BaseController.ts
@@ -1,14 +1,36 @@
-import type { IMidiOutputPort } from "../adapters";
+import { ContextTime, TransportState } from "@blibliki/transport";
+import { Engine } from "@/Engine";
 
 export abstract class BaseController {
-  protected outputPort: IMidiOutputPort;
+  protected engineId: string;
   protected isInDawMode = false;
 
-  constructor(outputPort: IMidiOutputPort) {
-    this.outputPort = outputPort;
+  constructor(engineId: string) {
+    this.engineId = engineId;
+    this.transport.addPropertyChangeCallback(
+      "state",
+      this.onStateChange.bind(this),
+    );
   }
 
   abstract enterDawMode(): Promise<void>;
-
   abstract exitDawMode(): Promise<void>;
+
+  start() {
+    void this.engine.start();
+  }
+
+  stop() {
+    this.engine.stop();
+  }
+
+  abstract onStateChange(state: TransportState, actionAt: ContextTime): void;
+
+  protected get transport() {
+    return Engine.getById(this.engineId).transport;
+  }
+
+  protected get engine() {
+    return Engine.getById(this.engineId);
+  }
 }
