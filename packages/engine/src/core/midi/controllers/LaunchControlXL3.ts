39f63286aedf2b18f8a149db63e753c384a1de7b
Michalis Zabaras <mikezaby@gmail.com>
2026-03-09 19:37:20 +0200

    Enreach information about colors and button for LaunchControlXL3

--- packages/engine/src/core/midi/controllers/LaunchControlXL3.ts
+++ packages/engine/src/core/midi/controllers/LaunchControlXL3.ts
@@ -4,22 +4,161 @@ import { BaseController } from "./BaseController";
 import type { MatchedControllerPorts } from "./ControllerMatcher";
 
 enum Color {
+  // ===== GRAYS =====
   Gray0 = 0,
+  Gray1 = 1,
+  Gray2 = 2,
+  Gray3 = 3,
+  Gray4 = 70,
+  Gray5 = 71,
+  Gray6 = 91,
+  Gray7 = 103,
+  Gray8 = 115,
+  Gray9 = 116,
+  Gray10 = 117,
+  Gray11 = 118,
+  Gray12 = 119,
+
+  // ===== REDS =====
   Red0 = 4,
-  Red6 = 120,
+  Red1 = 5,
+  Red2 = 6,
+  Red3 = 7,
+  Red4 = 72,
   Red5 = 106,
+  Red6 = 120,
+
+  // ===== ORANGES =====
+  Orange0 = 8,
+  Orange1 = 9,
+  Orange2 = 10,
+  Orange3 = 11,
+  Orange4 = 84,
   Orange5 = 96,
+  Orange6 = 107,
+  Orange7 = 108,
+  Orange8 = 121,
+  Orange9 = 126,
+
+  // ===== YELLOWS =====
+  Yellow0 = 12,
+  Yellow1 = 13,
+  Yellow2 = 14,
+  Yellow3 = 15,
+  Yellow4 = 73,
   Yellow5 = 74,
+  Yellow6 = 75,
+  Yellow7 = 85,
+  Yellow8 = 97,
+  Yellow9 = 98,
+  Yellow10 = 99,
+  Yellow11 = 109,
+  Yellow12 = 113,
+  Yellow13 = 124,
+  Yellow14 = 125,
+
+  // ===== GREENS =====
   Green0 = 16,
+  Green1 = 17,
+  Green2 = 18,
+  Green3 = 19,
+  Green4 = 20,
+  Green5 = 21,
+  Green6 = 22,
+  Green7 = 23,
+  Green8 = 64,
+  Green9 = 65,
   Green10 = 76,
+  Green11 = 86,
+  Green12 = 87,
+  Green13 = 88,
+  Green14 = 89,
   Green15 = 101,
+  Green16 = 102,
+  Green17 = 110,
+  Green18 = 111,
+  Green19 = 114,
+  Green20 = 122,
+  Green21 = 123,
+
+  // ===== TEALS =====
+  Teal0 = 24,
+  Teal1 = 25,
+  Teal2 = 26,
+  Teal3 = 27,
+  Teal4 = 28,
+  Teal5 = 29,
+  Teal6 = 30,
+  Teal7 = 31,
+  Teal8 = 68,
+  Teal9 = 69,
+
+  // ===== CYANS =====
+  Cyan0 = 32,
+  Cyan1 = 33,
+  Cyan2 = 34,
+  Cyan3 = 35,
+  Cyan4 = 36,
   Cyan5 = 37,
+  Cyan6 = 38,
+  Cyan7 = 39,
+  Cyan8 = 66,
+  Cyan9 = 67,
+  Cyan10 = 77,
+  Cyan11 = 78,
+  Cyan12 = 90,
+
+  // ===== BLUES =====
+  Blue0 = 40,
+  Blue1 = 41,
+  Blue2 = 42,
   Blue3 = 43,
+  Blue4 = 79,
+  Blue5 = 92,
+
+  // ===== INDIGOS =====
+  Indigo0 = 44,
+  Indigo1 = 45,
+  Indigo2 = 46,
+  Indigo3 = 47,
+  Indigo4 = 104,
+  Indigo5 = 112,
+
+  // ===== PURPLES =====
+  Purple0 = 48,
+  Purple1 = 49,
+  Purple2 = 50,
   Purple3 = 51,
+  Purple4 = 80,
+  Purple5 = 81,
+  Purple6 = 93,
+
+  // ===== PINKS =====
+  Pink0 = 52,
+  Pink1 = 53,
+  Pink2 = 54,
+  Pink3 = 55,
+  Pink4 = 56,
   Pink5 = 57,
+  Pink6 = 58,
+  Pink7 = 59,
+  Pink8 = 82,
+  Pink9 = 94,
+  Pink10 = 95,
+
+  // ===== BROWNS =====
+  Brown0 = 60,
+  Brown1 = 61,
+  Brown2 = 62,
+  Brown3 = 63,
+  Brown4 = 83,
+  Brown5 = 100,
+  Brown6 = 105,
+  Brown7 = 127,
 }
 
 enum Control {
+  // ===== ENCODERS =====
   Encoder0_0 = 13,
   Encoder0_1 = 14,
   Encoder0_2 = 15,
@@ -47,6 +186,17 @@ enum Control {
   Encoder2_6 = 35,
   Encoder2_7 = 36,
 
+  // ===== FADERS =====
+  Fader0 = 5,
+  Fader1 = 6,
+  Fader2 = 7,
+  Fader3 = 8,
+  Fader4 = 9,
+  Fader5 = 10,
+  Fader6 = 11,
+  Fader7 = 12,
+
+  // ===== CHANNEL BUTTONS =====
   ChannelButton0_0 = 37,
   ChannelButton0_1 = 38,
   ChannelButton0_2 = 39,
@@ -65,8 +215,19 @@ enum Control {
   ChannelButton1_6 = 51,
   ChannelButton1_7 = 52,
 
+  // ===== TRANSPORT =====
   Play = 116,
   Record = 118,
+
+  // ===== NAVIGATION =====
+  PageUp = 106,
+  PageDown = 107,
+  TrackPrev = 103,
+  TrackNext = 102,
+
+  // ===== MODIFIERS =====
+  Shift = 63,
+  Mode = 104,
 }
 
 export class LaunchControlXL3 extends BaseController {
