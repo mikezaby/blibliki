10c1c61f32de0bd8c02017ef941fb37c85482307
Michalis Zabaras <mikezaby@gmail.com>
2026-03-11 19:24:31 +0200

    Update Distortion faders

--- apps/grid/src/components/AudioModule/Distortion.tsx
+++ apps/grid/src/components/AudioModule/Distortion.tsx
@@ -1,21 +1,11 @@
-import { ModuleType, moduleSchemas } from "@blibliki/engine";
+import {
+  ModuleType,
+  ModuleTypeToPropsMapping,
+  moduleSchemas,
+} from "@blibliki/engine";
 import { Fader, type MarkProps } from "@blibliki/ui";
 import { ModuleComponent } from ".";
 import Container from "./Container";
-
-const DRIVE_MARKS: MarkProps[] = [
-  { value: 0, label: "Clean" },
-  { value: 3, label: "Crunch" },
-  { value: 6, label: "Heavy" },
-  { value: 10, label: "Extreme" },
-];
-
-const TONE_MARKS: MarkProps[] = [
-  { value: 200, label: "200Hz" },
-  { value: 1000, label: "1kHz" },
-  { value: 5000, label: "5kHz" },
-  { value: 20000, label: "20kHz" },
-];
 
 const MIX_MARKS: MarkProps[] = [
   { value: 0, label: "Dry" },
@@ -31,27 +21,35 @@ const Distortion: ModuleComponent<ModuleType.Distortion> = (props) => {
 
   const schema = moduleSchemas[ModuleType.Distortion];
 
+  const onChange = (
+    prop: keyof ModuleTypeToPropsMapping[ModuleType.Distortion],
+  ) => {
+    const update = updateProp(prop);
+
+    return (_: number, calcValue: number) => {
+      update(calcValue);
+    };
+  };
+
   return (
     <Container>
       <Fader
         name="Drive"
-        marks={DRIVE_MARKS}
         min={schema.drive.min}
         max={schema.drive.max}
         step={schema.drive.step}
         value={drive}
-        onChange={updateProp("drive")}
+        onChange={onChange("drive")}
       />
 
       <Fader
         name="Tone"
-        marks={TONE_MARKS}
         min={schema.tone.min}
         max={schema.tone.max}
         step={schema.tone.step}
         exp={schema.tone.exp}
         value={tone}
-        onChange={updateProp("tone")}
+        onChange={onChange("tone")}
       />
 
       <Fader
@@ -61,7 +59,7 @@ const Distortion: ModuleComponent<ModuleType.Distortion> = (props) => {
         max={schema.mix.max}
         step={schema.mix.step}
         value={mix}
-        onChange={updateProp("mix")}
+        onChange={onChange("mix")}
       />
     </Container>
   );
